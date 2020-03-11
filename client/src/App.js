c25fd6f770f6c3282813b2aec073e5e87ad01ebb
ram <fikriramadhan@gmail.com>
2020-03-11 10:03:52 +0700

    client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci nesciunt minus, quos accusamus, aperiam quas consequatur expedita dolore natus, molestiae placeat sequi hic quidem. Quidem minima quisquam similique amet commodi.
+    </div>
+  );
+}
+
+export default App;
